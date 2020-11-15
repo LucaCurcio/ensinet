18da600015d42f4da9e86a1f4f32fb9e6debf5dd
Cesar Guibo <cesarguibo0@gmail.com>
2020-11-14 23:02:23 -0300

    first commit

---
+++ upload_to_git.sh
@@ -0,0 +1,7 @@
+echo "# ensinet" >> README.md
+git init
+git branch -M main
+git add * 
+git commit -m "first commit"
+git remote add origin git@github.com:cesar-guibo/ensinet.git
+git push -u origin main
